d2ac1b96106af577629a6e19b05f51e5dffb53e6
Laith massoud <laythmassoud8@gmail.com>
2021-02-01 23:02:44 +0100

    create an component for the filter of the cards

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import newsletter from "../../images/newsletter.png";
+import arrow from "../../images/arrow-down.png";
+import hour from "../../images/hour-svgrepo-com.svg";
+const Filter = () => {
+  return (
+    <div>
+      <span>Filters : </span>
+      <span>
+        <img className="icon" src={newsletter} alt="newsletter-icon" />my
+        sources{" "}
+      </span>{" "}
+      <img className="icon" src={arrow} alt="arrow-icon" />
+      |
+      <span>
+        {" "}<img className="icon" src={hour} alt="hour-icon" /> past month
+      </span>
+      <img className="icon" src={arrow} alt="arrow-icon" />
+    </div>
+  );
+};
+
+export default Filter;
